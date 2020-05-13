b27a9f6b850f7a58a694724553862e0c80c4e7e4
Daniel Luca <lucadanielcostin@gmail.com>
2020-05-13 13:04:30 +0300

    Initial commit.

---
+++ coinpaprika.py
@@ -0,0 +1,3 @@
+# https://api.coinpaprika.com/v1/tickers/btc-bitcoin/historical?start=2017-02-15T05:15:00Z&end=2018-02-15T05:30:00Z&interval=5m
+
+# https://github.com/influxdata/influxdb-python
---
+++ nomics.py
@@ -0,0 +1,5 @@
+from nomics import Nomics
+nomics = Nomics("d92f3a1452e4686064b481ddd4f48c44")
+
+# https://p.nomics.com/cryptocurrency-bitcoin-api
+# https://api.nomics.com/v1/exchange-rates/history?key=demo-26240835858194712a4f8cc0dc635c7a&currency=ETH&start=2018-04-14T00%3A00%3A00Z&end=2019-05-14T00%3A00%3A00Z
